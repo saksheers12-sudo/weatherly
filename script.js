c94d3c2096e684de493251b713cd88e529ada9b9
Sakshee <saksheers12@gmail.com>
2026-08-11 17:07:54 +0000

    done

--- script.js
+++ script.js
@@ -202,9 +202,9 @@ function animate() {
     requestAnimationFrame(animate);
 
     // Gentle continuous Earth rotation
-    earth.rotation.y += 0.008;
-earth.rotation.x += 0.00045;
-earth.rotation.z += 0.00022;
+    earth.rotation.y += 0.004;
+earth.rotation.x += 0.00035;
+earth.rotation.z += 0.00012;
 
     // Keep atmosphere aligned with Earth
     atmosphere.rotation.y = earth.rotation.y;
